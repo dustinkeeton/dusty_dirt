c91230de001c96f6da0a94d1980a778d00ab9b9b
Dustin Keeton <dustin.keeton49@gmail.com>
2021-09-06 09:46:41 -0700

    feat: better performance; don't wait

--- dusty_dirt.py
+++ dusty_dirt.py
@@ -1,5 +1,4 @@
 #! /usr/bin/python3
-import time
 import yaml
 from bluepy.btle import Peripheral, Scanner, DefaultDelegate
 import paho.mqtt.client as mqtt
@@ -11,7 +10,9 @@ pumpReadWrite = "cbbb4e46-0dcf-11ec-82a8-0242ac130003"
 soilRead = "ba8cf798-0de1-11ec-82a8-0242ac130003"
 client = mqtt.Client('soil')
 
-global currentPeripheral
+global curPeripheral
+global curPumpReadWriteCharacteristic
+global curSoilReadCharacteristic
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -24,14 +25,14 @@ def on_disconnect(client, userdata,rc=0):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print("MQTT received for topic '"+msg.topic+"', with payload of "+str(msg.payload))
     if (msg.payload == b'ON'):
         turnOnPump()
+    # TODO: have this fn queue the action on the next connection to the appropriate device
 
 
 client.on_connect = on_connect
-# client.on_disconnect = on_disconnect
-client.on_message = on_message
+client.on_disconnect = on_disconnect
 client.username_pw_set(config['username'], config['password'])
 client.connect(config['client_ip'], config['client_port'], config['client_keep_alive'])
 
@@ -42,62 +43,59 @@ def getPeripheralCharacteristicByUUID(peripheral, uuid):
 
 def turnOnPump():
     print('Turning pump on')
-    client.publish('pump', "ON", retain=True)
     on = (1).to_bytes(1, byteorder="big")
-    pumpReadReadWriteCharacteristic = getPeripheralCharacteristicByUUID(currentPeripheral, pumpReadWrite)
-    pumpReadReadWriteCharacteristic.write(on, withResponse=True)
+    client.publish('pump', payload="ON", retain=True)
+    client.loop()
+    try:
+        curPumpReadWriteCharacteristic.write(on, withResponse=True)
+    except:
+        print("Error turning on pump")
+        client.publish('pump', payload="OFF", retain=True)
+        client.loop()
 
 def automateWatering(moisture):
     print('Determining actions...')
     if (moisture < config['moisture_threshold']):
-        print('Turning pump on')
-        on = (1).to_bytes(1, byteorder="big")
-        pumpReadReadWriteCharacteristic = getPeripheralCharacteristicByUUID(peripheral, pumpReadWrite)
-        pumpReadReadWriteCharacteristic.write(on, withResponse=True)
+        turnOnPump()
     else:
         print('No actions needed.')
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print ("Discovered device", dev.addr0)
-        elif isNewData:
-            print ("Received new data from"), dev.addr
+def filterDevice(dev):
+    name = str(dev.getValueText(9))
+    return name.find('Dusty Dirt Sensor') != -1
 
 # scanner = Scanner().withDelegate(ScanDelegate())
 # devices = scanner.scan(10.0)
 print('Scanning devices...')
 devices = Scanner().scan()
 
-for dev in devices:
-    name = str(dev.getValueText(9))
-    if name.find('Dusty Dirt Sensor') != -1 and dev.connectable:
-        print('Device found!')
-        print('Reading moisture values...')
-        # connect to peripheral and get service
+found = list(filter(filterDevice, devices))
+print(str(len(found))+' Device(s) found!')
+
+for dev in found:
+    if dev.connectable:
+        name = str(dev.getValueText(9))
+        # connect to peripheral and get service and characteristics
+        print('==========================================================================================')
+        print('Connecting to '+name+'...')
         peripheral = Peripheral(dev)
-        currentPeripheral = peripheral
+        curPeripheral = peripheral
         service = peripheral.getServiceByUUID(soilService)
-
-        # read soil characteristic and publish to mqtt
         soilReadCharacteristic = getPeripheralCharacteristicByUUID(peripheral, soilRead)
-        pumpReadWriteCharacteristic = getPeripheralCharacteristicByUUID(peripheral, pumpReadWrite)
+        curPumpReadWriteCharacteristic = getPeripheralCharacteristicByUUID(peripheral, pumpReadWrite)
+        client.message_callback_add('pump/set', on_message)
 
+        print('Reading values...')
         while True:
-            valBytes = soilReadCharacteristic.read()
-            moisture = int.from_bytes(valBytes, "big")
-            client.publish('sensor/soil', moisture, retain=True)
+           # read soil characteristic and publish to mqtt
+            moisture = int.from_bytes(soilReadCharacteristic.read(), "big")
+            client.publish('sensor/soil', payload=moisture, retain=True)
 
-            poopBytes = pumpReadWriteCharacteristic.read()
-            pumpState = int.from_bytes(poopBytes, "big")
-            client.publish('pump', "ON" if pumpState else "OFF", retain=True)
+            pumpState = int.from_bytes(curPumpReadWriteCharacteristic.read(), "big")
+            client.publish('pump', payload="ON" if pumpState else "OFF", retain=True)
 
-            print(name + ' = ' + str(moisture))
+            print('moisture: ' + str(moisture))
             if (config['automate_watering']):
                 automateWatering(moisture)
-            time.sleep(config['read_interval'])
 
 client.loop_stop()
